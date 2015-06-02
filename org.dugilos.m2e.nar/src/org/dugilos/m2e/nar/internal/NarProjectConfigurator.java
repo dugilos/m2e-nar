a9f6bed24d743e92bee68409cafda6f4494ab188
dugilos <jef.blanc@gmail.com>
2015-06-02 21:42:17 +0200

    Add licence GPLv3.

--- org.dugilos.m2e.nar/src/org/dugilos/m2e/nar/internal/NarProjectConfigurator.java
+++ org.dugilos.m2e.nar/src/org/dugilos/m2e/nar/internal/NarProjectConfigurator.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2015 Jean-François Blanc
+ * 
+ * This file is part of m2e nar plugin.
+ * 
+ * m2e nar plugin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * m2e nar plugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with m2e nar plugin. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.dugilos.m2e.nar.internal;
 
 import java.util.ArrayList;
